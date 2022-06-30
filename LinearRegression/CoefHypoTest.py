27c36377544a7632dbe99482f6f1de8d5ac1f305
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-06-30 23:51:29 +0300

    For the beta coefficient, hypothesis test test statistics method was created

---
+++ LinearRegression/CoefHypoTest.py
@@ -0,0 +1,12 @@
+import math
+from Regression.LinearRegression import RegressionParameters
+import numpy as np
+
+def B_hypot(x, y, beta):
+    """
+    Test Statistics for B
+    :param x: List of input levels
+    :param y: List of output
+    :param beta : Claimed beta value
+    :return : Test statistic with t-distribution calculated with the requested beta value
+    """
