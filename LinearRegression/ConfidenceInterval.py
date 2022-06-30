e2103242cf61dc95cde7aa939e69ccc82610ac86
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-01 00:00:38 +0300

    Confidence interval method was created.

---
+++ LinearRegression/ConfidenceInterval.py
@@ -0,0 +1,5 @@
+from scipy.stats import t  # for Values of critical points in T_table
+from Regression.LinearRegression import RegressionParameters as rp
+from Regression.LinearRegression.CoefHypoTest import *
+import math as mt
+import numpy as np
