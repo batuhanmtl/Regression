ee06d302aa4427a54a25de3f14d67d292f65578b
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-01 00:20:28 +0300

    Weighted least squares method has been created.

--- LinearRegression/WeightedLeastSquares.py
+++ LinearRegression/WeightedLeastSquares.py
@@ -1,2 +1,11 @@
 from scipy import linalg
 import numpy as np
+
+def Weighted_Squares(x, y):
+    """
+    A function that allows us to obtain the alpha and beta values,
+        which are the coefficients of the weighted linear regression equation.
+    :param x: List of input levels
+    :param y: List of output
+    :return: Alpha and beta values, which are the coefficients of the weighted linear regression equation.
+    """
