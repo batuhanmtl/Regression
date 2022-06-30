e73727e20ee94484d886834402487790423eadff
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-01 00:01:25 +0300

    Confidence interval method was created for beta(B).

--- LinearRegression/ConfidenceInterval.py
+++ LinearRegression/ConfidenceInterval.py
@@ -3,3 +3,13 @@ from Regression.LinearRegression import RegressionParameters as rp
 from Regression.LinearRegression.CoefHypoTest import *
 import math as mt
 import numpy as np
+
+def B_Conf_Interval(x, y, alpha):
+    """
+    Confidence interval estimator for beta
+    alpha = significance level
+    :param x: List of input levels
+    :param y: List of output
+    :param alpha: significance level
+    :return: Confidence interval for beta
+    """
