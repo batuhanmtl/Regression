870b026dbb186ae8197889719a8dfaa8646446bd
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-01 00:17:21 +0300

    coefficient of determination (R2) method was created.

--- LinearRegression/R2.py
+++ LinearRegression/R2.py
@@ -1,2 +1,10 @@
 from Regression.LinearRegression import RegressionParameters as rp
 import numpy as np
+
+def R2(x, y):
+    """
+    It shows the amount of change in the response variables explained by different input values.
+    :param x: List of input levels
+    :param y: List of output
+    :return: Rate of change in response variables
+    """
