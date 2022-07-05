cb4e166cdf1f714b338111120af04a09eaae4e57
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-06 00:50:50 +0300

    Multiple Coefficient of Determination -R2- Method Created

--- MultipleLinearRegression/MultipleLinReg.py
+++ MultipleLinearRegression/MultipleLinReg.py
@@ -202,3 +202,5 @@ def Multiple_R2(data, x_cols_name=None, y_col=''):
     Y = np.array(Y)
 
     Y = Y.reshape(len(data), 1)
+
+    ssr = Multiple_SSr(data=data, x_cols_name=x_cols_name, y_col=y_col)
