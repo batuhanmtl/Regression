d3300e096e82e56ce661d641a33f698c442afc93
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-06 00:14:47 +0300

    Multiple Regression module created

--- MultipleLinearRegression/MultipleLinReg.py
+++ MultipleLinearRegression/MultipleLinReg.py
@@ -2,3 +2,12 @@ import numpy as np
 import pandas as pd
 
 def Multiple_Regression(data, x_cols_name=None, y_col=''):
+    """
+       Function that calculates
+       the coefficients of the Multiple Linear Regression equation.
+
+       :param data: must be a data frame
+       :param x_cols_name: must be a list with column names of non-dependent Variables.
+       :param y_col: column name of the dependent variable
+       :return: numpy.ndarray -array of beta coefficients in the regression equation-
+       """
