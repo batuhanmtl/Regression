b2a531c41cfd81091c9a6bf40d0310d7cbdae15f
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-06 14:12:02 +0300

    Estimator method of mean response created

--- MultipleLinearRegression/Prediction.py
+++ MultipleLinearRegression/Prediction.py
@@ -4,3 +4,5 @@ from scipy.stats import t
 from LinearRegression.MultipleLinearRegression import Multiple_SSr, Multiple_Regression
 from math import sqrt as sq
 
+def Predict(data, alpha=0.05, x_cols_name=None, y_col='', x_value=None):
+
